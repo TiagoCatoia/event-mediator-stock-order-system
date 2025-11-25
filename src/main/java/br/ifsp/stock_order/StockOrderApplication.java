b9266b90e91ee376dba910bd8698cc49a840edf3
FelipeSantosLourenco <ppandinha4@gmail.com>
2025-11-25 17:14:20 -0300

    feat: add spring system base

---
+++ src/main/java/br/ifsp/stock_order/StockOrderApplication.java
@@ -0,0 +1,13 @@
+package br.ifsp.stock_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifsp/stock_order/StockOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.stock_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
