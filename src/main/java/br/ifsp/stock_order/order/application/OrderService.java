0280949d40dd8f47618271fe2384f128872f2463
FelipeSantosLourenco <ppandinha4@gmail.com>
2025-11-30 18:08:24 -0300

    feat: add publishOrderCreatedEvent on createOrder

--- src/main/java/br/ifsp/stock_order/order/application/OrderService.java
+++ src/main/java/br/ifsp/stock_order/order/application/OrderService.java
@@ -1,6 +1,8 @@
 package br.ifsp.stock_order.order.application;
 
+import br.ifsp.stock_order.common.events.OrderCreatedEvent;
 import br.ifsp.stock_order.customer.infrastructure.CustomerRepository;
+import br.ifsp.stock_order.infrastructure.RabbitMQConfig;
 import br.ifsp.stock_order.order.api.dto.CreateOrderRequest;
 import br.ifsp.stock_order.order.api.dto.OrderItemResponse;
 import br.ifsp.stock_order.order.api.dto.OrderResponse;
@@ -8,7 +10,10 @@ import br.ifsp.stock_order.order.domain.OrderStatus;
 import br.ifsp.stock_order.order.infrastructure.OrderEntity;
 import br.ifsp.stock_order.order.infrastructure.OrderItemEntity;
 import br.ifsp.stock_order.order.infrastructure.OrderRepository;
+import br.ifsp.stock_order.product.infrastructure.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,10 +25,14 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final CustomerRepository customerRepository;
+    private final ProductRepository productRepository;
+    private final RabbitTemplate rabbitTemplate;
 
-    public OrderService(OrderRepository orderRepository, CustomerRepository customerRepository) {
+    public OrderService(OrderRepository orderRepository, CustomerRepository customerRepository, ProductRepository productRepository, RabbitTemplate rabbitTemplate) {
         this.orderRepository = orderRepository;
         this.customerRepository = customerRepository;
+        this.productRepository = productRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public List<OrderResponse> findOrders(){
@@ -33,6 +42,7 @@ public class OrderService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public OrderResponse createOrder(CreateOrderRequest request) {
         var customer = customerRepository.findById(request.customerId())
                 .orElseThrow(() -> new EntityNotFoundException("Customer not found: " + request.customerId()));
@@ -45,25 +55,52 @@ public class OrderService {
 
         double totalOrderPrice = 0.0;
 
-//        for (var itemRequest : request.items()) {
-//            var product = productRepository.findById(itemRequest.productId())
-//                    .orElseThrow(() -> new EntityNotFoundException("Product not found: " + itemRequest.productId()));
-//
-//            double itemTotal = product.getPrice() * itemRequest.quantity();
-//
-//            var orderItem = new OrderItemEntity();
-//            orderItem.setOrder(order);
-//            orderItem.setProduct(product);
-//            orderItem.setUnitPrice(itemTotal);
-//
-//            order.getOrderItems().add(orderItem);
-//            totalOrderPrice += itemTotal;
-//        }
+        for (var itemRequest : request.items()) {
+            var product = productRepository.findById(itemRequest.productId())
+                    .orElseThrow(() -> new EntityNotFoundException("Product not found: " + itemRequest.productId()));
+
+            double itemTotal = product.getPrice() * itemRequest.quantity();
+
+            var orderItem = new OrderItemEntity();
+            orderItem.setOrder(order);
+            orderItem.setProduct(product);
+            orderItem.setQuantity(itemRequest.quantity());
+            orderItem.setUnitPrice(itemTotal);
+            orderItem.setOrderStatus(OrderStatus.CREATED);
+
+            order.getOrderItems().add(orderItem);
+            totalOrderPrice += itemTotal;
+        }
 
         order.setTotalPrice(totalOrderPrice);
         OrderEntity savedOrder = orderRepository.save(order);
 
+        publishOrderCreatedEvent(savedOrder);
+
         return toOrderResponse(savedOrder);
+    }
+
+    private void publishOrderCreatedEvent(OrderEntity savedOrder) {
+        List<OrderCreatedEvent.OrderItemData> itemsData = savedOrder.getOrderItems().stream()
+                .map(item -> new OrderCreatedEvent.OrderItemData(
+                        item.getProduct().getId(),
+                        item.getQuantity()
+                )).toList();
+
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                savedOrder.getId(),
+                savedOrder.getCustomer().getId(),
+                savedOrder.getTotalPrice(),
+                savedOrder.getStatus().toString(),
+                savedOrder.getCreatedAt(),
+                itemsData
+        );
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_ORDER,
+                RabbitMQConfig.RK_ORDER_CREATED,
+                event
+        );
     }
 
     private OrderResponse toOrderResponse(OrderEntity entity) {
