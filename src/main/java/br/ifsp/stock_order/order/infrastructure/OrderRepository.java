670cde48714680938f62becf1f4c263dbafaec4a
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-11-30 13:17:57 -0300

    refactor: add @EntityGraph to findAll method of OrderRepository

--- src/main/java/br/ifsp/stock_order/order/infrastructure/OrderRepository.java
+++ src/main/java/br/ifsp/stock_order/order/infrastructure/OrderRepository.java
@@ -1,10 +1,15 @@
 package br.ifsp.stock_order.order.infrastructure;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
+    @Override
+    @EntityGraph(attributePaths = {"orderItems", "customer", "orderItems.product"})
+    List<OrderEntity> findAll();
 }
