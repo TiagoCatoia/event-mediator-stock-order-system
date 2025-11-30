13de98016763e7bf20819190fae88737c33a39c0
Tiago Catoia <tiagocsz@hotmail.com>
2025-11-30 12:02:53 -0300

    feat: add customer repository

---
+++ src/main/java/br/ifsp/stock_order/customer/infrastructure/CustomerRepository.java
@@ -0,0 +1,10 @@
+package br.ifsp.stock_order.customer.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
+}
