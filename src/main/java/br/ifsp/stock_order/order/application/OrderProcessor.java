8fffacae74e323b785daa0bd233a017e07c75553
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-12-03 20:29:52 -0300

    fix: Circuit breaker fallback to handleOrderCancel method

--- src/main/java/br/ifsp/stock_order/order/application/OrderProcessor.java
+++ src/main/java/br/ifsp/stock_order/order/application/OrderProcessor.java
@@ -2,8 +2,8 @@ package br.ifsp.stock_order.order.application;
 
 import br.ifsp.stock_order.common.command.CancelOrderCommand;
 import br.ifsp.stock_order.common.config.RabbitMQConfig;
-import br.ifsp.stock_order.common.event.OrderCancelFailedEvent;
 import br.ifsp.stock_order.common.infrastructure.EmailService;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -18,37 +18,28 @@ public class OrderProcessor {
     private final RabbitTemplate rabbitTemplate;
     private final EmailService emailService;
 
-    @CircuitBreaker(name = "orderCancelCircuitBreaker", fallbackMethod = "fallbackCancelOrder")
+    @CircuitBreaker(name = "stock-api", fallbackMethod = "fallbackCancelOrder")
     @RabbitListener(queues = RabbitMQConfig.QUEUE_ORDER_CANCEL)
     public void handleOrderCancel(CancelOrderCommand command) {
         System.out.println("PROCESSADOR ORDER recebeu uma ordem de cancelamento. Pedido: " + command.orderId());
-
-        try {
-            orderService.cancelOrder(command.orderId());
-            System.out.println("PROCESSADOR ORDER concluiu o cancelamento do pedido: " + command.orderId());
-
-        } catch (Exception e) {
-
-            OrderCancelFailedEvent event = new OrderCancelFailedEvent(
-                    command.orderId(),
-                    e.getMessage());
-
-            rabbitTemplate.convertAndSend(
-                    RabbitMQConfig.EXCHANGE_ORDER,
-                    RabbitMQConfig.RK_ORDER_CANCEL,
-                    command
-            );
-        }
+        orderService.cancelOrder(command.orderId());
     }
 
+    // Open
+    public void fallbackCancelOrder(CancelOrderCommand command, CallNotPermittedException ex) {
+        System.err.println("Erros múltiplos ao processar o pedido, devolvendo dinheiro...");
+//        String subject = "Falha persistente ao cancelar pedido " + command.orderId();
+//        String body = "O sistema de cancelamento está indisponível. Circuito aberto.";
+//        emailService.sendSimpleEmail("user@gmail.com", subject, body);
+    }
+
+    // Closed
     public void fallbackCancelOrder(CancelOrderCommand command, Throwable throwable) {
-        String subject = "Falha persistente ao cancelar pedido " + command.orderId();
-        String body = "O pedido " + command.orderId() + " não pôde ser cancelado. Erro: " + throwable.getMessage();
-
-        sendEmailAlert(subject, body);
-    }
-
-    private void sendEmailAlert(String subject, String body) {
-        emailService.sendSimpleEmail("user@gmail.com", subject, body);
+        System.err.println("Erro ao processar cancelamento do pedido, tentando novamente");
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_ORDER,
+                RabbitMQConfig.RK_ORDER_CANCEL,
+                command
+        );
     }
 }
