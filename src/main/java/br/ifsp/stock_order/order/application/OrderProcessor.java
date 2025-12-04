11a6159ddc316c254c47fa8172faa09b4da067a4
Tiago Catoia <tiagocsz@hotmail.com>
2025-12-03 22:00:14 -0300

    refactor: add span to create order flow

--- src/main/java/br/ifsp/stock_order/order/application/OrderProcessor.java
+++ src/main/java/br/ifsp/stock_order/order/application/OrderProcessor.java
@@ -5,6 +5,7 @@ import br.ifsp.stock_order.common.config.RabbitMQConfig;
 import br.ifsp.stock_order.common.infrastructure.EmailService;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -17,29 +18,68 @@ public class OrderProcessor {
     private final OrderService orderService;
     private final RabbitTemplate rabbitTemplate;
     private final EmailService emailService;
+    private final Tracer tracer;
 
     @CircuitBreaker(name = "stock-api", fallbackMethod = "fallbackCancelOrder")
     @RabbitListener(queues = RabbitMQConfig.QUEUE_ORDER_CANCEL)
     public void handleOrderCancel(CancelOrderCommand command) {
-        System.out.println("PROCESSADOR ORDER recebeu uma ordem de cancelamento. Pedido: " + command.orderId());
         orderService.cancelOrder(command.orderId());
+    }
+
+    private void sendEmail(CancelOrderCommand command) {
+        var emailSpan = tracer.nextSpan().name("sendEmail").start();
+        try {
+            String subject = "Falha persistente ao cancelar pedido " + command.orderId();
+            String body = "O sistema de cancelamento está indisponível. Circuito aberto.";
+            emailService.sendSimpleEmail("user@gmail.com", subject, body);
+
+            emailSpan.tag("subject", subject);
+            emailSpan.tag("recipient", "user@gmail.com");
+            emailSpan.tag("status", "sent");
+        } catch (Exception exception) {
+            emailSpan.error(exception);
+            throw exception;
+        } finally {
+            emailSpan.end();
+        }
     }
 
     // Open
     public void fallbackCancelOrder(CancelOrderCommand command, CallNotPermittedException ex) {
-        System.err.println("Erros múltiplos ao processar o pedido, devolvendo dinheiro...");
-//        String subject = "Falha persistente ao cancelar pedido " + command.orderId();
-//        String body = "O sistema de cancelamento está indisponível. Circuito aberto.";
-//        emailService.sendSimpleEmail("user@gmail.com", subject, body);
+        var fallbackOpenSpan = tracer.nextSpan().name("cancelOrderFallbackOpen").start();
+
+        try {
+            fallbackOpenSpan.tag("orderId", command.orderId().toString());
+            fallbackOpenSpan.tag("circuitBreakerState", "open");
+
+            sendEmail(command);
+        }
+        catch (Exception exception) {
+            fallbackOpenSpan.error(exception);
+            throw exception;
+        } finally {
+            fallbackOpenSpan.end();
+        }
     }
 
     // Closed
     public void fallbackCancelOrder(CancelOrderCommand command, Throwable throwable) {
-        System.err.println("Erro ao processar cancelamento do pedido, tentando novamente");
-        rabbitTemplate.convertAndSend(
-                RabbitMQConfig.EXCHANGE_ORDER,
-                RabbitMQConfig.RK_ORDER_CANCEL,
-                command
-        );
+        var fallbackClosedSpan = tracer.nextSpan().name("cancelOrderFallbackClosed").start();
+
+        try {
+            rabbitTemplate.convertAndSend(
+                    RabbitMQConfig.EXCHANGE_ORDER,
+                    RabbitMQConfig.RK_ORDER_CANCEL,
+                    command
+            );
+
+            fallbackClosedSpan.tag("orderId", command.orderId().toString());
+            fallbackClosedSpan.tag("circuitBreakerState", "closed");
+        } catch (Exception exception) {
+            fallbackClosedSpan.error(exception);
+            throw exception;
+        } finally {
+            fallbackClosedSpan.end();
+        }
     }
 }
